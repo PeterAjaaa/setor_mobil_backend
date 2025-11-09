4d30716bca222f1a892cda2cf19c01ef6ad25fbd
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-09 16:51:16 +0700

    REFACTOR: Move LoginRequest and LoginResponse DTO struct from user.go to auth.go under dto/

---
+++ dto/auth.go
@@ -0,0 +1,10 @@
+package dto
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
--- dto/user.go
+++ dto/user.go
@@ -27,15 +27,6 @@ type UserResponseRequest struct {
 	Email string `json:"email"`
 }
 
-type LoginRequest struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
-}
-
-type LoginResponse struct {
-	Token string `json:"token"`
-}
-
 type Genders string
 
 const (
