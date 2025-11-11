a949a6a4a52b3fab14f71d0edaa13375fbd9a2d1
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-12 00:49:32 +0700

    Add JwtClaims interface, due to GenerateJWT() function refactor. Implement JwtClaims interface to Users model

--- models/interface.go
+++ models/interface.go
@@ -3,3 +3,9 @@ package models
 type Model interface {
 	TableName() string
 }
+
+type JwtClaims interface {
+	GetID() uint
+	GetEmail() string
+	GetName() string
+}
--- models/user.go
+++ models/user.go
@@ -21,8 +21,22 @@ type Users struct {
 	Kecamatan  string
 	Occupation string
 	Email      string
+	// TODO: Turn this back on when Orders has been implemented
+	// Orders     *[]Orders
 }
 
 func (user Users) TableName() string {
 	return reflect.TypeOf(user).Name()
+}
+
+func (user Users) GetID() uint {
+	return user.ID
+}
+
+func (user Users) GetEmail() string {
+	return user.Email
+}
+
+func (user Users) GetName() string {
+	return user.Name
 }
