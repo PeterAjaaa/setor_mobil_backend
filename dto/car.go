ee1b3a87fb5e6964637a2f396f30a2e43c995eb1
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-12 00:37:59 +0700

    Add new CarCreationRequest struct for car creation DTO

--- dto/car.go
+++ dto/car.go
@@ -1,5 +1,17 @@
 package dto
 
+type CarCreationRequest struct {
+	// Assuming the possible shortest commercially available registration number as 'A 1'
+	RegistrationNum string `json:"registration_num" validate:"required,min=3"`
+	Brand           string `json:"brand" validate:"required,min=3"`
+	Model           string `json:"model" validate:"required,min=3"`
+	Year            uint16 `json:"year" validate:"required"`
+	PricePerDay     uint32 `json:"price_per_day" validate:"required"`
+	Status          string `json:"status" validate:"required,oneof=Available Rented Maintenance"`
+	Description     string `json:"description"`
+	ImageURL        string `json:"image_url" validate:"required"`
+}
+
 type CarResponseRequest struct {
 	ID              uint   `json:"id"`
 	RegistrationNum string `json:"registration_num"`
