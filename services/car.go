55e67b84c1de8f19adbc72c5d24cc1357cb28f84
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-12 00:42:07 +0700

    Add CreateNewCar() function to facilitate new car creation. Add checks for the HTTP methods used.

--- services/car.go
+++ services/car.go
@@ -6,14 +6,22 @@ import (
 	"net/http"
 
 	"github.com/PeterAjaaa/setor_mobil_backend/dto"
+	"github.com/PeterAjaaa/setor_mobil_backend/helper"
 	"github.com/PeterAjaaa/setor_mobil_backend/logger"
 	"github.com/PeterAjaaa/setor_mobil_backend/models"
+	"gorm.io/gorm"
 )
 
 func (h *ServiceHandler) GetCarById(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	logger.LOG.Debug("Getting car in GetCarById() function...")
 
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
+
 	var car models.Cars
 	var response dto.CarResponseRequest
 
@@ -48,9 +56,15 @@ func (h *ServiceHandler) GetCarById(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *ServiceHandler) GetAllCars(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	logger.LOG.Debug("Getting all cars in GetAllCars() function...")
 
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
+
 	var cars []models.Cars
 	var response []dto.CarResponseRequest
 
@@ -84,4 +98,70 @@ func (h *ServiceHandler) GetAllCars(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(response)
+}
+
+func (h *ServiceHandler) CreateNewCar(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	logger.LOG.Debug("Creating new car in CreateNewCar() function...")
+
+	var req dto.CarCreationRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		logger.LOG.Error(err.Error())
+		return
+	}
+
+	adminID, ok := helper.GetUserID(r.Context())
+
+	if !ok {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	car := models.Cars{
+		RegistrationNum: req.RegistrationNum,
+		Brand:           req.Brand,
+		Model:           req.Model,
+		Year:            req.Year,
+		PricePerDay:     req.PricePerDay,
+		Status:          req.Status,
+		Description:     req.Description,
+		ImageURL:        req.ImageURL,
+		CreatedByID:     adminID,
+	}
+
+	db := helper.GetDB()
+	txErr := db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(&car).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if txErr != nil {
+		logger.LOG.Error("Error inserting data, transaction rolled back:")
+		http.Error(w, txErr.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		logger.LOG.Info("Successfully created new car with transaction!")
+	}
+
+	response := dto.CarResponseRequest{
+		ID:              car.ID,
+		RegistrationNum: car.RegistrationNum,
+		Brand:           car.Brand,
+		Model:           car.Model,
+		Year:            car.Year,
+		PricePerDay:     car.PricePerDay,
+		Status:          car.Status,
+		Description:     car.Description,
+		ImageURL:        car.ImageURL,
+	}
+	json.NewEncoder(w).Encode(response)
+
 }
