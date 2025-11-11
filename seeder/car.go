ea4695aebe5ed3f93b102cfdc798cf1f12edfdd0
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-12 00:39:22 +0700

    Add CreatedByID field to the Car seeder

--- seeder/car.go
+++ seeder/car.go
@@ -12,6 +12,7 @@ var carSeed = []models.Cars{
 		Status:          "Available",
 		Description:     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec nulla leo. Fusce sed nibh ipsum. Nunc id ex ut ex suscipit laoreet id sit amet mi. Cras lobortis sodales ligula, ut fermentum justo faucibus vitae. Suspendisse potenti. Nunc tempor felis non commodo auctor. Vivamus congue justo non nisi vehicula interdum.",
 		ImageURL:        "https://picsum.photos/seed/avanza/800/600",
+		CreatedByID:     1,
 	},
 	{
 		RegistrationNum: "D 5678 EOF",
@@ -22,5 +23,6 @@ var carSeed = []models.Cars{
 		Status:          "Rented",
 		Description:     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus leo mollis augue aliquam, quis hendrerit nunc vehicula. Sed finibus est sit amet gravida consectetur. Phasellus convallis, purus eget luctus posuere, nibh felis suscipit urna, aliquet congue massa neque non massa. Donec vitae lacus sed elit bibendum sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut finibus congue libero. Fusce sed efficitur mauris. Phasellus gravida orci vel aliquet mollis. Donec sit amet maximus purus. Curabitur eget libero condimentum, consectetur ante sit amet, fringilla erat. Mauris a sapien ut ipsum posuere venenatis. Aenean vitae urna volutpat, commodo nibh et, viverra augue.",
 		ImageURL:        "https://picsum.photos/seed/civic/800/600",
+		CreatedByID:     2,
 	},
 }
