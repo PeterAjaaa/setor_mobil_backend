37d517494d99e6e09aaccab3e2ef718b3061e3b6
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-13 09:20:49 +0700

    Add Orders field to User DTO. Change Gender field type from Genders to string.

--- dto/user.go
+++ dto/user.go
@@ -2,6 +2,8 @@ package dto
 
 import (
 	"time"
+
+	"github.com/PeterAjaaa/setor_mobil_backend/models"
 )
 
 type UserCreationRequest struct {
@@ -9,7 +11,7 @@ type UserCreationRequest struct {
 	Password   string    `json:"password" validate:"required,min=8"`
 	Birthdate  time.Time `json:"birthdate" validate:"required"`
 	Birthplace string    `json:"birthplace" validate:"required"`
-	Gender     Genders   `json:"gender" validate:"required,oneof=Laki-laki Perempuan"`
+	Gender     string    `json:"gender" validate:"required,oneof=Laki-laki Perempuan"`
 	// Assuming the possible shortest address as 'Jl a no 1'
 	Address    string `json:"address" validate:"required,min=9"`
 	RT         uint16 `json:"rt" validate:"required,min=1,max=3"`
@@ -22,14 +24,8 @@ type UserCreationRequest struct {
 }
 
 type UserResponseRequest struct {
-	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID     uint             `json:"id"`
+	Name   string           `json:"name"`
+	Email  string           `json:"email"`
+	Orders *[]models.Orders `json:"orders"`
 }
-
-type Genders string
-
-const (
-	LAKI_LAKI Genders = "Laki-laki"
-	PEREMPUAN Genders = "Perempuan"
-)
