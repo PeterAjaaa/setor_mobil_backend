5fa82c91598f09ab0a6c728ebbffe2d4241c9e28
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-13 09:21:45 +0700

    Change Status field in APIReponse struct to int, following HTTP status codes

--- models/api.go
+++ models/api.go
@@ -1,7 +1,7 @@
 package models
 
 type APIResponse struct {
-	Status  string `json:"status"`
+	Status  int    `json:"status"`
 	Message string `json:"message,omitempty"`
 	Data    any    `json:"data,omitempty"`
 }
