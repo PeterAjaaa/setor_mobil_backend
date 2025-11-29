f6c999125e17b19609b3b7a436122a6a441542ab
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-11-29 17:43:57 +0700

    Modify SeedTable() function to seed only if table if empty

--- helper/seeder.go
+++ helper/seeder.go
@@ -15,21 +15,33 @@ func SeedTable[T models.Model](db *gorm.DB, data []T) {
 	}
 
 	tableName := data[0].TableName()
-	logger.LOG.Info(fmt.Sprintf("Seeding %s table...", tableName))
+	logger.LOG.Info(fmt.Sprintf("Checking %s table for existing data...", tableName))
+
+	var count int64
+	if err := db.Model(data[0]).Count(&count).Error; err != nil {
+		logger.LOG.Error(fmt.Sprintf("Error checking %s table count: %s", tableName, err))
+		return
+	}
+
+	if count > 0 {
+		logger.LOG.Info(fmt.Sprintf("Table %s already contains %d records. Skipping seeding.", tableName, count))
+		return
+	}
+
+	logger.LOG.Info(fmt.Sprintf("Seeding %s table (empty)...", tableName))
 
 	txErr := db.Transaction(func(tx *gorm.DB) error {
-		for _, user := range data {
-			if err := tx.Create(&user).Error; err != nil {
+		for _, item := range data {
+			if err := tx.Create(&item).Error; err != nil {
 				return err
 			}
 		}
-
 		return nil
 	})
 
 	if txErr != nil {
 		logger.LOG.Warn(fmt.Sprintf("Error seeding data to table %s, transaction rolled back. ERROR: %s", tableName, txErr))
 	} else {
-		logger.LOG.Info(fmt.Sprintf("Successfully seeded %s table data", tableName))
+		logger.LOG.Info(fmt.Sprintf("Successfully seeded %d records to %s table", len(data), tableName))
 	}
 }
